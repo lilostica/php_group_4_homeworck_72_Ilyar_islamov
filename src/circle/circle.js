afd5799d57adef03833fe50216effed68fa6fa6b
Ilyar Islamov <kennyx360@gmail.com>
2020-08-23 13:52:22 +0600

    added circle component

---
+++ src/circle/circle.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Circle = props =>
+{
+  return(
+      <div className="circle">
+          <h3>{props.number}</h3>
+      </div>
+  )
+};
+
+export default Circle;
