47e403bcfb26bd096c1c282cafd8aba65fbf3a0a
Ilyar Islamov <kennyx360@gmail.com>
2020-08-24 15:09:28 +0600

    added range function and App class

--- src/App.js
+++ src/App.js
@@ -1,27 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import Circle from "./circle/circle";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function range() {
+    const array = [];
+    for (let i = 5; i <= 36; i++) {
+        array.push(i);
+    }
+    const five = [];
+    for (let i = 0; i <= 4; i++) {
+        five.push(array[Math.floor(Math.random() * array.length)]);
+    }
+    five.sort(function (a, b) {
+        return a - b
+    });
+    const rand = Math.floor(Math.random() * five.length);
+    return five[rand];
 }
+
+class App extends Component {
+    state = {
+        numbers: [
+            {number: range()},
+            {number: range()},
+            {number: range()},
+            {number: range()},
+            {number: range()},
+        ]
+    };
+    handleChangeNumber = () =>
+    {
+        this.setState({numbers: [
+                {number: range()},
+                {number: range()},
+                {number: range()},
+                {number: range()},
+                {number: range()},
+            ]});
+    };
+    render()
+    {
+        return (
+            <div className="App">
+                <div>
+                    <button onClick={this.handleChangeNumber}>range</button>
+                    <Circle number={this.state.numbers[0].number}/>
+                    <Circle number={this.state.numbers[1].number}/>
+                    <Circle number={this.state.numbers[2].number}/>
+                    <Circle number={this.state.numbers[3].number}/>
+                    <Circle number={this.state.numbers[4].number}/>
+                </div>
+            </div>
+        );
+    }
+}
+
 
 export default App;
